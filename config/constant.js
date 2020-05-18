c5c659966c2d04d999e09cd22ae3407ddc692521
Neil Francis Ramirez Hipona <nferocious76@gmail.com>
2020-05-18 17:47:29 +0800

    add constant.js

---
+++ config/constant.js
@@ -0,0 +1,61 @@
+'use strict';
+
+/** SERVER */
+exports.SERVER_MAINTENANCE = 'Maintenance: Server is undergoing service upgrade. Please return later'
+
+/** APP RESOURCES */
+exports.RESOURCE_CREATE_FAILED = 'Could not create resource';
+exports.RESOURCE_CREATE_SUCCESS = 'Resource added';
+exports.RESOURCE_UPDATE_FAILED = 'Could not update resource';
+exports.RESOURCE_UPDATE_SUCCESS = 'Resource updated';
+exports.RESOURCE_FETCH_FAILED = 'Could not fetch resource';
+exports.RESOURCE_FETCH_SUCCESS = 'Resource fetched';
+exports.RESOURCE_DISABLE_FAILED = 'Could not disable resource';
+exports.RESOURCE_DISABLE_SUCCESS = 'Resource disabled';
+exports.RESOURCE_ENABLE_FAILED = 'Could not enable resource';
+exports.RESOURCE_ENABLE_SUCCESS = 'Resource enabled';
+
+/** APP PERMISSION */
+exports.PERMISSION_CREATE_FAILED = 'Could not create resource permission';
+exports.PERMISSION_CREATE_SUCCESS = 'Resource permission added';
+exports.PERMISSION_UPDATE_FAILED = 'Could not update resource permission';
+exports.PERMISSION_UPDATE_SUCCESS = 'Resource permission updated';
+exports.PERMISSION_DELETE_FAILED = 'Could not delete resource permission';
+exports.PERMISSION_DELETE_SUCCESS = 'Resource permission deleted';
+exports.PERMISSION_FETCH_FAILED = 'Could not fetch resource permission';
+exports.PERMISSION_FETCH_SUCCESS = 'Resource permission fetched';
+
+/** APP MAINTENANCE */
+exports.MAINTENANCE_INFO_FETCH_FAILED = 'Could not retrieve maintenance information';
+exports.MAINTENANCE_INFO_FETCH_SUCCESS = 'Maintenance information retrieved';
+exports.MAINTENANCE_SET_FAILED = 'Could not set maintenance window';
+exports.MAINTENANCE_SET_SUCCESS = 'Maintenance window active';
+exports.MAINTENANCE_HISTORY_FETCH_FAILED = 'Could not fetch maintenance history';
+exports.MAINTENANCE_HISTORY_FETCH_SUCCESS = 'Maintenance history fetched';
+
+/** ROLE */
+exports.ROLE_CREATE_FAILED = 'Could not create role';
+exports.ROLE_CREATE_SUCCESS = 'Role created';
+exports.ROLE_UPDATE_FAILED = 'Could not update role';
+exports.ROLE_UPDATE_SUCCESS = 'Role updated';
+exports.ROLE_FETCH_FAILED = 'Could not fetch roles';
+exports.ROLE_FETCH_SUCCESS = 'Roles fetched';
+exports.ROLE_DELETE_FAILED = 'Could not delete roles';
+exports.ROLE_DELETE_SUCCESS = 'Roles deleted';
+
+/** PERMISSION **/
+exports.PERMISSION_DENIED           = 'Permission denied';
+exports.UNAUTHORIZED_REQUEST        = 'Unauthorized request';
+exports.SERVER_ERROR                = 'Server error';
+exports.DATABASE_CONN_ERROR         = 'Connection error';
+exports.SERVICE_UNAVAILABLE         = 'Service unavailable';
+exports.FORBIDDEN_REQUEST           = 'Forbidden request';
+exports.SERVER_MAINTENANCE          = 'Maintenance: Server is undergoing service upgrade. Please return later';
+
+/** USER */
+exports.USER_SIGNIN_FAILED          = 'Invalid email and/or password';
+exports.USER_SIGNIN_SUCCESS         = 'Signed in success';
+exports.USER_SIGNED_OUT             = 'You have logged out';
+exports.USER_CREATE_FAILED          = 'Could not create user';
+exports.USER_CREATE_SUCCESS         = 'User created';
+
